fd60df2d1c9030ef1320e289dfb42326b54768cc
Kate Richerson <kate.e.richerson@noaa.gov>
2022-06-15 13:58:36 -0700

    Removing ASHOP stuff, making bycatchspp_col a character argument,

--- do_ratio_multi.R
+++ do_ratio_multi.R
@@ -8,45 +8,27 @@
 #bycatchunit = The name of the column with the measure of bycatch to be expanded. Defaults to dis_mt, but could be counts, etc. 
 #management_groups = do we want to calculate ratios by managment grouping? Defaults to TRUE.
 
-do_ratio_multi <- function(ob_dat, strata, expfactor = "tgt_mt", bycatchspp_col = species, bycatchspp, bycatchunit = "dis_mt", management_groups = TRUE)
+
+do_ratio_multi_test <- function(ob_dat = ob, strata = c("sector", "gear", "year"), expfactor = "tgt_mt", bycatchspp_col = "species", bycatchspp, bycatchunit = "dis_mt", management_groups = FALSE) #haul_id trip_id drvid
 {
   require(dplyr)
   require(tidyr)
   require(rlang)
   require(janitor)
+  options(dplyr.summarise.inform = FALSE)
   
-  bycatchspp_col <- enquo(bycatchspp_col)
-  bycatchspp_string <- gsub("~","", deparse(bycatchspp_col)) #make into string for grouping 
+  ratio.se <- function(x.mean, y.mean, x.se, y.se){ sqrt(  (x.mean/y.mean)^2 *  (  (x.se/x.mean)^2  +  (y.se/y.mean)^2  -  ( (x.se/x.mean)^2 * (y.se/y.mean)^2 ) ) ) }
   
-  #Identify whether the input data is ASHOP or WCGOP
-  if(any(ob_dat$sector %in% c("CATCHER-PROCESSOR", "MOTHERSHIP", "ATSEA TRIBAL"))){program <- "ASHOP"} else {
-    program <- "WCGOP"
-  }
-  
-  #Do a little bit of data prep. We could also calculate tgt_mt in here by sector at some point?
-  if(program == "WCGOP")
-  {
-    ob_data <- clean_names(ob_dat) %>% 
-      filter(datatype == "Analysis Data")
-  }
-  
-  #For ASHOP, we put in NAs for trip_id and just remove later. 
-  if(program == "ASHOP")
-  {
-    ob_data <- clean_names(ob_dat) %>% 
-      mutate(tgt_mt = pwht_mt) %>% 
-      mutate(trip_id = NA)
-  }
   
   #Do we want management groupings or not? Seems easiest (but long) to do it this way,
   if(!management_groups)
   {
     #First, summarise the expansion factor data by strata.
-    obdf_expf <- ob_data %>% 
-      filter(!is.na(haul_id)) %>% #removes any trip without catch
+    obdf_expf <- ob_dat %>% 
+      filter(!is.na(haul_id) & 
+               datatype == "Analysis Data") %>% #removes any trip without catch
       #Haul-level summary
       group_by_at(c(strata, "haul_id", "trip_id", "drvid")) %>% 
-      ###summarise(expf_haul = sum(!!exp_fac, na.rm=T)) %>% !!sym(var)
       summarise(expf_haul = sum(!!sym(expfactor), na.rm=T)) %>%
       ungroup() %>%
       #Get the expansion factor by strata
@@ -64,50 +46,52 @@ do_ratio_multi <- function(ob_dat, strata, expfactor = "tgt_mt", bycatchspp_col 
       dplyr::select(-whatev)
     
     #Get unique combination of bycatch species, hauls, to make sure we account for hauls with 0 bycatch below. This seemed simpler than dealing with nested tidyr::expand.
-    unique_hauls_sp <- ob_data %>% 
+    unique_hauls_sp <- ob_dat %>%
       filter(!is.na(haul_id)) %>% #removes any trip without catch
-      dplyr::select(!!!syms(strata), haul_id, trip_id, drvid) %>% 
-      distinct() %>% 
+      dplyr::select(!!!syms(strata), haul_id, trip_id, drvid) %>%
+      distinct() %>%
       #This is a hacky way of making sure each row contains strata, trip, haul, drvid, and bycatch species
-      mutate(all_byc_species = paste(bycatchspp, sep="", collapse=",")) %>% 
-      tidyr::separate(all_byc_species, bycatchspp, sep = ",") %>% 
-      tidyr::gather(!!bycatchspp_col, whatev, all_of(bycatchspp)) %>% 
+      mutate(all_byc_species = paste(bycatchspp, sep="", collapse=",")) %>%
+      tidyr::separate(all_byc_species, bycatchspp, sep = ",") %>%
+      tidyr::gather(!!bycatchspp_col, whatev, all_of(bycatchspp)) %>%
       dplyr::select(-whatev)
     
-    #Now summarise bycatch data by strata
-    obdf_byc <- ob_data %>% 
-      #Haul-level summary  
+    # #Now summarise bycatch data by strata
+    obdf_byc <- ob_dat %>%
+      #Haul-level summary
       #Filter to only species of interest
-      filter(!!bycatchspp_col %in% bycatchspp) %>% #
+      filter(!!sym(bycatchspp_col) %in% bycatchspp) %>% #
       #Joining all the unique haul/bycatch species combinations ensures that hauls with 0 catch are preserved.
-      right_join(unique_hauls_sp) %>% 
-      group_by_at(c(strata, bycatchspp_string, "haul_id", "trip_id", "drvid")) %>% 
-      summarise(byc_haul = sum(!!sym(bycatchunit), na.rm=T)) %>% 
-      ungroup() %>% 
+      right_join(unique_hauls_sp, by = c(strata, "haul_id", "trip_id", "drvid", bycatchspp_col)) %>%
+      group_by_at(c(strata, bycatchspp_col, "haul_id", "trip_id", "drvid")) %>%
+      summarise(byc_haul = sum(!!sym(bycatchunit), na.rm=T)) %>%
+      ungroup() %>%
       #Summarise the observed bycatch by strata, including the number of hauls that encountered bycatch
-      group_by_at(c(strata, bycatchspp_string)) %>% 
+      group_by_at(c(strata, bycatchspp_col)) %>%
       summarise(total_byc = sum(byc_haul),
                 mean_byc = mean(byc_haul),
                 se_byc = sqrt(var(byc_haul)/length(byc_haul)),
                 n_hauls_byc = n_distinct(haul_id[byc_haul > 0]))
     
     
-    obdf <- obdf_expf %>% 
-      full_join(obdf_byc) %>% 
-      ungroup() %>% 
+    obdf <- obdf_expf %>%
+      full_join(obdf_byc, by = c(strata, bycatchspp_col)) %>%
+      ungroup() %>%
       mutate(byc_ratio = total_byc / total_expf,
              byc_se = ratio.se(mean_byc, mean_expf, se_byc, se_expf),
-             byc_ratio_lower = byc_ratio - 1.96 * byc_se, 
+             byc_ratio_lower = byc_ratio - 1.96 * byc_se,
              byc_ratio_upper = byc_ratio + 1.96 * byc_se,
              pct_hauls_byc = round((n_hauls_byc/n_obs_hauls)*100, 2))
   } #end if !management_groups statement
+  
   
   #If we want management groups we do things slightly differently
   if(management_groups)
   {
     #First, summarise the expansion factor data by strata.
-    obdf_expf <- ob_data %>% 
-      filter(!is.na(haul_id)) %>% #removes any trip without catch
+    obdf_expf <- ob_dat %>% 
+      filter(!is.na(haul_id) & 
+               datatype == "Analysis Data") %>% #removes any trip without catch
       #Haul-level summary
       group_by_at(c(strata, "haul_id", "trip_id", "drvid")) %>% 
       ###summarise(expf_haul = sum(!!exp_fac, na.rm=T)) %>% 
@@ -128,12 +112,12 @@ do_ratio_multi <- function(ob_dat, strata, expfactor = "tgt_mt", bycatchspp_col 
       dplyr::select(-whatev)
     
     #Get the groupings we may need to preserve (any that are present in the data and contain our species of interest)
-    spp_grps <- filter(ob_data, !!bycatchspp_col %in% bycatchspp) %>% 
+    spp_grps <- filter(ob_dat, !!sym(bycatchspp_col) %in% bycatchspp) %>% 
       dplyr::select(!!bycatchspp_col, grouping) %>% 
       distinct()
     
     #Get unique combination of bycatch species, hauls, management groups to make sure we account for hauls with 0 bycatch below. This seemed simpler than dealing with nested tidyr::expand.
-    unique_hauls_sp <- ob_data %>% 
+    unique_hauls_sp <- ob_dat %>% 
       filter(!is.na(haul_id)) %>% #removes any trip without catch
       dplyr::select(!!!syms(strata), haul_id, trip_id, drvid) %>% 
       distinct() %>% 
@@ -142,23 +126,22 @@ do_ratio_multi <- function(ob_dat, strata, expfactor = "tgt_mt", bycatchspp_col 
       tidyr::separate(all_byc_species, bycatchspp, sep = ",") %>% 
       tidyr::gather(!!bycatchspp_col, whatev, bycatchspp) %>% 
       dplyr::select(-whatev)%>% 
-      full_join(spp_grps)
+      full_join(spp_grps, by = bycatchspp_col)
     
     #Now summarise bycatch data by strata
-    obdf_byc <- ob_data %>% 
+    obdf_byc <- ob_dat %>% 
       #Haul-level summary  
       #Filter to only species of interest
-      filter(!!bycatchspp_col %in% bycatchspp) %>% #
+      filter(!!sym(bycatchspp_col) %in% bycatchspp) %>%
       #Joining all the unique haul/bycatch species combinations ensures that hauls with 0 catch are preserved.
-      right_join(unique_hauls_sp) %>% 
-      group_by_at(c(strata, "grouping", bycatchspp_string, "haul_id", "trip_id", "drvid")) %>% 
-      ###summarise(byc_haul = sum(!!byc_unit, na.rm=T)) %>% 
+      right_join(unique_hauls_sp,by = c(strata, "haul_id", "trip_id", "drvid", bycatchspp_col,"grouping")) %>% 
+      group_by_at(c(strata, "grouping", bycatchspp_col, "haul_id", "trip_id", "drvid")) %>% 
       summarise(byc_haul = sum(!!sym(bycatchunit), na.rm=T)) %>%
       ungroup() %>% 
       #Fill in 0s for strata without observed bycatch, for completeness. "Nesting" should return only strata combinations present in the data. 
       #tidyr::complete(tidyr::nesting(!!!syms(strata)), spid_eqv, fill = list(byc = 0)) %>% 
       #Summarise the observed bycatch by strata, including number of hauls with bycatch
-      group_by_at(c(strata, "grouping", bycatchspp_string)) %>% 
+      group_by_at(c(strata, "grouping", bycatchspp_col)) %>% 
       summarise(total_byc = sum(byc_haul),
                 mean_byc = mean(byc_haul),
                 se_byc = sqrt(var(byc_haul)/length(byc_haul)),
@@ -166,7 +149,7 @@ do_ratio_multi <- function(ob_dat, strata, expfactor = "tgt_mt", bycatchspp_col 
     
     
     obdf <- obdf_expf %>% 
-      full_join(obdf_byc) %>% 
+      full_join(obdf_byc, by = c(strata, bycatchspp_col)) %>% 
       ungroup() %>% 
       mutate(byc_ratio = total_byc / total_expf,
              byc_se = ratio.se(mean_byc, mean_expf, se_byc, se_expf),
@@ -174,13 +157,6 @@ do_ratio_multi <- function(ob_dat, strata, expfactor = "tgt_mt", bycatchspp_col 
              byc_ratio_upper = byc_ratio + 1.96 * byc_se, 
              pct_hauls_byc = round((n_hauls_byc/n_obs_hauls)*100, 2))
   } #end if management_groups statement
-  
-  #Remove trips from ASHOP output because it's not meaningful
-  if(program == "ASHOP")
-  {
-    obdf <- obdf %>% 
-      dplyr::select(-n_obs_trips)
-  }
   
   return(as.data.frame(obdf))
   
